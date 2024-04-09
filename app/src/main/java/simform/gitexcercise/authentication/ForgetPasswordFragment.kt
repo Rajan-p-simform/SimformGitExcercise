a0a4ef5bf753f36556dded4fc00f4ebd97cec713
Rajan Patel <rajan12233445@gmail.com>
2024-04-09 19:47:54 +0530

    Onboarding: Authentication Flow

---
+++ app/src/main/java/simform/gitexcercise/authentication/ForgetPasswordFragment.kt
@@ -0,0 +1,23 @@
+package simform.gitexcercise.authentication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import simform.gitexcercise.android.R
+import simform.gitexcercise.android.databinding.FragmentForgetPasswordBinding
+
+class ForgetPasswordFragment : Fragment() {
+
+    private lateinit var binding: FragmentForgetPasswordBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(layoutInflater,R.layout.fragment_forget_password,container,false)
+        return binding.root
+    }
+}
