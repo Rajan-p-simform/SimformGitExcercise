65988355fd4d5ef948e67089992de24bf09f3028
Rajan Patel <rajan12233445@gmail.com>
2024-04-11 09:32:14 +0530

    UNT-T23102: profile screen

---
+++ app/src/main/java/simform/gitexcercise/profile/ProfileFragment.kt
@@ -0,0 +1,23 @@
+package simform.gitexcercise.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import simform.gitexcercise.android.R
+import simform.gitexcercise.android.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(layoutInflater,R.layout.fragment_profile,container,false)
+        return binding.root
+    }
+}
